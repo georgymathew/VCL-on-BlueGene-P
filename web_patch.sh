6d37ac76fdb11157d81c68673fb956a30be2475f
georgymathew <georgy@ncsu.edu>
2013-07-26 11:36:09 -0400

    Create web_patch.sh

---
+++ web_patch.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+echo "Please enter the password you configured for MySQL service on ANL Login Node"
+read username
+
+echo "Please enter the password you configured for MySQL service on ANL Login Node"
+read pass
+
+#editing the username for the interaction scripts
+
+sed -i 's/username/'$username'/g' /var/www/html/vcl/jb.php
+sed -i 's/username/'$username'/g' /var/www/html/vcl/jp.php
+sed -i 's/username/'$username'/g' /var/www/html/vcl/.ht-inc/t.sh
+sed -i 's/username/'$username'/g' /var/www/html/vcl/.ht-inc/t1.sh
+sed -i 's/username/'$username'/g' /var/www/html/vcl/.ht-inc/t2.sh
+sed -i 's/username/'$username'/g' /var/www/html/vcl/.ht-inc/template.sh
+sed -i 's/username/'$username'/g' /var/www/html/vcl/.ht-inc/hpc.sh
+
+#editing the password for the interaction scripts
+sed -i 's/mysqlpass/'$pass'/g' /var/www/html/vcl/jb.php
+sed -i 's/mysqlpass/'$pass'/g' /var/www/html/vcl/jp.php
+sed -i 's/mysqlpass/'$pass'/g' /var/www/html/vcl/.ht-inc/template.sh
+sed -i 's/mysqlpass/'$pass'/g' /var/www/html/vcl/.ht-inc/hpc.sh
+
+echo "Script Completed"
