7283ed244af70858bb0af46df28c3c93ae488abb
georgymathew <georgy@ncsu.edu>
2013-07-26 12:24:58 -0400

    Update web_patch.sh

--- web_patch.sh
+++ web_patch.sh
@@ -16,6 +16,7 @@ sed -i 's/username/'$username'/g' /var/www/html/vcl/.ht-inc/template.sh
 sed -i 's/username/'$username'/g' /var/www/html/vcl/.ht-inc/hpc.sh
 
 #editing the password for the interaction scripts
+#not needed as the user goes as a passwordless access
 sed -i 's/mysqlpass/'$pass'/g' /var/www/html/vcl/jb.php
 sed -i 's/mysqlpass/'$pass'/g' /var/www/html/vcl/jp.php
 sed -i 's/mysqlpass/'$pass'/g' /var/www/html/vcl/.ht-inc/template.sh
